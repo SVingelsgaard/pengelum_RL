2131a196d46b00747dae5447ad382632d531c57f
SVingelsgaard <sigurd@vingelsgaard.no>
2022-02-17 22:59:16 +0100

    added RL model

--- main.py
+++ main.py
@@ -28,6 +28,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.optimizers import Adam
+
+
+
 #window settings
 """Config.set('graphics', 'window_state', 'maximized')
 Config.set('graphics', 'fullscreen', '1')"""
@@ -145,6 +151,15 @@ class GUI(App):
         self.sliderLast = self.slider.value/10
 
         self.pengelum.theta += self.pengelum.rotVel * self.readCYCLETIME+ float(self.sliderResult)
+
+
+    def build_model(states, actions):
+        model = Sequential()
+        model.add(Flatten(input_shape=(1,states)))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(actions, activation='linear'))
+        return model
 
     def updateGraph(self):
         
